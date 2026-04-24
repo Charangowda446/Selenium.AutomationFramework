aec1ad771409a895eb6d8ad766d74c010d7705ed
Admin <Admin@DESKTOP-0PVBUFH>
2026-04-24 14:50:30 +0530

    Duck Number committed

---
+++ src/test/java/JAVA/Duck_Number.java
@@ -0,0 +1,28 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Duck_Number {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        System.out.println(n);
+		int count=0;
+		while(n!=0)
+		{
+			int rem=n%10;
+			System.out.println(rem);
+			if(rem==0)
+			{
+				count++;
+			}
+			n=n/10;
+		}
+		if(count>0)
+			System.out.println("Its a duck number");
+		else
+			System.out.println("Not a duck number");	
+	}
+}
